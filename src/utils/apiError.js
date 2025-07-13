f61ec72e47988cb8c3feb10a25a8bacb63a164e2
Nayan Patidar <b24cs1047@iitj.ac.in>
2025-07-13 19:51:58 +0530

    created utility for api error and an async handler

---
+++ src/utils/apiError.js
@@ -0,0 +1,23 @@
+class apiError extends Error{
+    contructor(
+        statusCode,
+        message= "Something went wrong!!",
+        errors = [],
+        stack = "",
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.errors = errors
+        this.data = null
+        this.success = false;
+
+        if(stack){
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.contructor)
+        }
+    }
+} 
+
+export {apiError}
