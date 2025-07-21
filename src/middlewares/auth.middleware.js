f933085903d739f4ddb7f39eb2f2c42567ff2f6c
Nayan Patidar <b24cs1047@iitj.ac.in>
2025-07-21 20:06:53 +0530

    add user account management endpoints

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,12 +7,12 @@ import { User } from "../models/user.models.js";
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        console.log(token)
+        // console.log(token)
         if(!token){
             throw new apiError(401, "Unauthorized Request");
         }
 
-        const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
 
